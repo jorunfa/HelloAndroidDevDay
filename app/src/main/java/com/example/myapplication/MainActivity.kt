94d623d6d7f0028302a22caca0e9113ed3b33145
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2019-02-28 18:45:37 +0100

    feat: Add old school hello world

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloText = findViewById<TextView>(R.id.textView)
+        val nameInput = findViewById<EditText>(R.id.editText)
+
+        val world = getString(R.string.world)
+        val helloWorld = getString(R.string.hello_something, world)
+
+        helloText.text = helloWorld
+
+        nameInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val newText = if (s?.length == 0) helloWorld else getString(R.string.hello_something, s)
+                helloText.text = newText
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
     }
 }
